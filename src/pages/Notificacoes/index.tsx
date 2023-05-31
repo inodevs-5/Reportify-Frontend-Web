a9c906013af036edc1cc605f381fc66694bd5ffc
Usuario <juliamaria215@gmail.com>
2023-05-30 22:20:34 -0300

    adicionando página de notificações

--- src/pages/Notificacoes/index.tsx
+++ src/pages/Notificacoes/index.tsx
@@ -1,22 +1,42 @@
+import React from 'react';
 import Menu from "../../components/menus";
-import "../../styles/global.css";
-// import { useState } from "react";
+//import "../../styles/global.css";
+import { useState } from "react";
+import "./notificacoes.css";
 
+interface Notification {
+  title: string;
+  
+  datetime: Date;
+}
 
-function Notificacao() {
+const notifications: Notification[] = [
+  { title: 'Mensagem recebida', datetime: new Date() },
+  { title: 'Atualização de RO 01',  datetime: new Date() },
+  { title: 'Mensagem recebida',  datetime: new Date() },
+ 
+];
 
+const Notificacao: React.FC = () => {
   return (
-    <div className="flex flex-wrap flex-row">
+    <><div className="flex flex-wrap flex-row">
       <Menu></Menu>
       <div id="conteudo" className="mt-16  bg-white flex-1 ">
         <div className="p-10 flex items-center flex-col">
-        <h1>Notificações</h1>
-
+          <h1>Notificações</h1>
         </div>
-    </div>
-    </div>
+      </div>
+    </div><div id="conteudo2">
+
+        {notifications.map((notification, index) => (
+          <div id="notificacoes" key={index}>
+            <h2>{notification.title}</h2>
+            <p>{notification.datetime.toLocaleString()}</p>
+            {index !== notifications.length - 1 && <hr />}
+          </div>
+        ))}
+      </div></>
   );
 };
 
-
-export default Notificacao
+export default Notificacao;
