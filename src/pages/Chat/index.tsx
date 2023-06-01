51124e4c9b485a2a76fbeae0b7e3b7c0a15c6e2c
Usuario <juliamaria215@gmail.com>
2023-05-31 23:50:17 -0300

    adicionando input de mensagem, botão de enviar e parte rolavel

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import './Chat.css';
+import React, { useState } from 'react';
+import './chat.css';
 import Menu from "../../components/menus";
 
 const Chat: React.FC = () => {
-  const messages = [
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([
     { content: 'Olá!', sender: 'user' },
     { content: 'Olá! Como posso ajudar?', sender: 'bot' },
     { content: 'Estou com um problema no meu pedido.', sender: 'user' },
     { content: 'Qual é o problema? Posso te ajudar.', sender: 'bot' },
-  ];
+  ]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputText.trim() !== '') {
+      const newMessage = { content: inputText, sender: 'user' };
+      setMessages([...messages, newMessage]);
+      setInputText('');
+    }
+  };
 
   return (
 
@@ -20,9 +33,11 @@ const Chat: React.FC = () => {
               </div>
           </div>
       </div>
+
       <div id="conteudo2">
         <div className="chat-container">
-              {messages.map((message, index) => (
+         <div className="messages-container">
+              {messages.map((message: { sender: string; content: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, index: React.Key | null | undefined) => (
                   <div
                       key={index}
                       className={`message ${message.sender === 'user' ? 'user' : 'bot'}`}
@@ -30,7 +45,19 @@ const Chat: React.FC = () => {
                       {message.content}
                   </div>
               ))}
-          </div></div></>
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Digite sua mensagem..."
+              value={inputText}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleSendMessage}>Enviar</button>
+         </div>
+       </div>
+      </div>
+    </>
   );
 };
 
